379c6d7bd2a4b57cb6bdaf015a959e6674d796b4
MaximI <makcimka1477@yandex.ru>
2023-05-15 12:43:04 +0300

    update key

---
+++ README.md
@@ -0,0 +1,9 @@
+Weather App
+
+Weather and astro forecast: current day, 10 days. Map geolocation.
+
+Api: Weather Api, DaData Api, 2gis MapGL Api
+
+Stack: HTML, SASS, JavaScript, ReactJS, Redux
+
+Netlify: https://weather-m-i-portfolio.netlify.app
--- src/components/features/map/Map.js
+++ src/components/features/map/Map.js
@@ -6,63 +6,64 @@ import { string } from "prop-types";
 
 import { load } from "@2gis/mapgl";
 
-const GIS_KEY = "cf331a9b-5c40-4b09-800c-627c1cff10f3";
+// const GIS_KEY = "cf331a9b-5c40-4b09-800c-627c1cff10f3"; // previous key
+const GIS_KEY = "80e73b39-61f0-4f73-bec3-cae04c7a869d";
 
 const Map = () => {
-  const { latitude, longitude } = useSelector((state) => state.coords);
+	const { latitude, longitude } = useSelector(state => state.coords);
 
-  useEffect(() => {
-    let map;
-    let marker;
-    load()
-      .then((mapglAPI) => {
-        map = new mapglAPI.Map("map-container", {
-          center: [longitude, latitude],
-          zoom: 13,
-          key: GIS_KEY,
-        });
+	useEffect(() => {
+		let map;
+		let marker;
+		load()
+			.then(mapglAPI => {
+				map = new mapglAPI.Map("map-container", {
+					center: [longitude, latitude],
+					zoom: 13,
+					key: GIS_KEY,
+				});
 
-        return mapglAPI;
-      })
-      .then((mapglAPI) => {
-        marker = new mapglAPI.Marker(map, {
-          coordinates: map.getCenter(),
-          label: {
-            text: `Вы здесь`,
-          },
-        });
-      });
-    return () => {
-      map && map.destroy();
-      marker && marker.destroy();
-    };
-  }, [latitude, longitude]);
+				return mapglAPI;
+			})
+			.then(mapglAPI => {
+				marker = new mapglAPI.Marker(map, {
+					coordinates: map.getCenter(),
+					label: {
+						text: `Вы здесь`,
+					},
+				});
+			});
+		return () => {
+			map && map.destroy();
+			marker && marker.destroy();
+		};
+	}, [latitude, longitude]);
 
-  return (
-    <div className="wrapper">
-      <div id="map" className="map" style={{ height: "100%" }}>
-        <MapWrapper />
-      </div>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<div id="map" className="map" style={{ height: "100%" }}>
+				<MapWrapper />
+			</div>
+		</div>
+	);
 };
 
 export default Map;
 
 const MapWrapper = memo(
-  () => {
-    return (
-      <div
-        id="map-container"
-        className="map-container"
-        style={{ width: "100%", height: "100%" }}
-      ></div>
-    );
-  },
-  () => true
+	() => {
+		return (
+			<div
+				id="map-container"
+				className="map-container"
+				style={{ width: "100%", height: "100%" }}
+			></div>
+		);
+	},
+	() => true,
 );
 
 Map.propTypes = {
-  width: string,
-  height: string,
+	width: string,
+	height: string,
 };
