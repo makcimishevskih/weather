8aa3c2ebda2fa90412107ae235278393b0bc4f21
MaximI <makcimka1477@yandex.ru>
2023-07-11 17:39:28 +0300

    media query refactoring

--- src/components/features/map/Map.js
+++ src/components/features/map/Map.js
@@ -1,36 +1,36 @@
-import "./Map.scss";
+import './Map.scss';
 
-import { memo, useEffect } from "react";
-import { useSelector } from "react-redux";
-import { string } from "prop-types";
+import { memo, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { string } from 'prop-types';
 
-import { load } from "@2gis/mapgl";
+import { load } from '@2gis/mapgl';
 
 // const GIS_KEY = "cf331a9b-5c40-4b09-800c-627c1cff10f3"; // previous key
-const GIS_KEY = "80e73b39-61f0-4f73-bec3-cae04c7a869d";
+const GIS_KEY = '80e73b39-61f0-4f73-bec3-cae04c7a869d';
 
 const Map = () => {
-	const { latitude, longitude } = useSelector(state => state.coords);
+	const { latitude, longitude } = useSelector((state) => state.coords);
 
 	useEffect(() => {
 		let map;
 		let marker;
 		load()
-			.then(mapglAPI => {
-				map = new mapglAPI.Map("map-container", {
+			.then((mapglAPI) => {
+				map = new mapglAPI.Map('map-container', {
 					center: [longitude, latitude],
 					zoom: 13,
-					key: GIS_KEY,
+					key: GIS_KEY
 				});
 
 				return mapglAPI;
 			})
-			.then(mapglAPI => {
+			.then((mapglAPI) => {
 				marker = new mapglAPI.Marker(map, {
 					coordinates: map.getCenter(),
 					label: {
-						text: `Вы здесь`,
-					},
+						text: `Вы здесь`
+					}
 				});
 			});
 		return () => {
@@ -40,8 +40,12 @@ const Map = () => {
 	}, [latitude, longitude]);
 
 	return (
-		<div className="wrapper">
-			<div id="map" className="map" style={{ height: "100%" }}>
+		<div className='wrapper'>
+			<div
+				id='map'
+				className='map'
+				style={{ height: '100%' }}
+			>
 				<MapWrapper />
 			</div>
 		</div>
@@ -54,16 +58,16 @@ const MapWrapper = memo(
 	() => {
 		return (
 			<div
-				id="map-container"
-				className="map-container"
-				style={{ width: "100%", height: "100%" }}
+				id='map-container'
+				className='map-container'
+				style={{ width: '100%', height: '100%' }}
 			></div>
 		);
 	},
-	() => true,
+	() => true
 );
 
 Map.propTypes = {
 	width: string,
-	height: string,
+	height: string
 };
